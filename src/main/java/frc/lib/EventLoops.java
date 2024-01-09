0426389af1f1d668475e9cf9f3977819e7f72c4e
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2024-01-08 19:21:25 -0600

    Update EventLoops.java

--- src/main/java/frc/lib/EventLoops.java
+++ src/main/java/frc/lib/EventLoops.java
@@ -3,6 +3,6 @@ package frc.lib;
 import edu.wpi.first.wpilibj.event.EventLoop;
 
 public class EventLoops {
-    public static final EventLoop oncePerSec = new EventLoop();
-    public static final EventLoop oncePerMin = new EventLoop();
+    public static final IsolatedEventLoop oncePerSec = new IsolatedEventLoop();
+    public static final IsolatedEventLoop oncePerMin = new IsolatedEventLoop();
 }
