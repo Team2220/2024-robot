fc53680657f35d06738cd872a9d60b37ea4eb330
Jithin <123132870+notJithin@users.noreply.github.com>
2024-01-16 20:52:35 -0600

    added codriver controller

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,6 +15,7 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+    public static final int kCodriverControllerPort = 1;
   }
   public static final class LimelightConfig {
     public static final String LEFT_NAME = "limelight-left";
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -30,6 +30,8 @@ public class RobotContainer {
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
+      private final CommandXboxController m_codriverController =
+      new CommandXboxController(OperatorConstants.kCodriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
