17ef8e8f0405499b7940eb9a3f282184616ac517
Jithin <123132870+notJithin@users.noreply.github.com>
2024-01-16 20:54:30 -0600

    stop logging joystick warning

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -32,6 +33,8 @@ public class Robot extends TimedRobot {
     addPeriodic(EventLoops.oncePerSec::poll, 1);
     addPeriodic(EventLoops.oncePerMin::poll, 60); 
     Fault.setupDefaultFaults();
+    // Stop logging for missing joysticks
+    DriverStation.silenceJoystickConnectionWarning(true);
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
