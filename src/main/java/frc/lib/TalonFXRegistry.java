4256d985d046518ba6a7251210fed6ba34919888
nerdguy22 <danielson.asher@gmail.com>
2024-01-18 20:08:39 -0600

    added TalonFX

---
+++ src/main/java/frc/lib/TalonFXRegistry.java
@@ -0,0 +1,13 @@
+package frc.lib;
+
+import java.util.ArrayList;
+
+public class TalonFXRegistry {
+    private static ArrayList<TalonFX> talonFXs = new ArrayList<>(); 
+
+    
+
+    public static void register(TalonFX talonFXs) {
+        talonFXs.add(talonFXs);
+    }
+}
