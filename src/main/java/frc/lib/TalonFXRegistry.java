1c47bfbe8c27566035fb5bf1a72b0777f249ca48
nerdguy22 <danielson.asher@gmail.com>
2024-01-18 20:10:12 -0600

    Update TalonFXRegistry.java

--- src/main/java/frc/lib/TalonFXRegistry.java
+++ src/main/java/frc/lib/TalonFXRegistry.java
@@ -4,9 +4,7 @@ import java.util.ArrayList;
 
 public class TalonFXRegistry {
     private static ArrayList<TalonFX> talonFXs = new ArrayList<>(); 
-
     
-
     public static void register(TalonFX talonFXs) {
         talonFXs.add(talonFXs);
     }
