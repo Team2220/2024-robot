84f2d5ab092c4ed212f889da826d084801d093b5
notJithin <123132870+notJithin@users.noreply.github.com>
2024-01-25 19:52:04 -0600

    AUTONOMOUSE

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -2,6 +2,9 @@ package frc.robot;
 
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.controller.HolonomicDriveController;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -11,8 +14,10 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -21,8 +26,11 @@ import static edu.wpi.first.math.util.Units.inchesToMeters;
 
 import java.util.function.DoubleSupplier;
 
-
 import com.kauailabs.navx.frc.AHRS;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -35,9 +43,36 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  */
 
 public class DriveTrain extends SubsystemBase {
+
     public DriveTrain() {
         SmartDashboard.putData("Field", m_field);
+        AutoBuilder.configureHolonomic(
+                this::getPose, // Robot pose supplier
+                this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+                this::getSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+                this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+                new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your
+                                                 // Constants class
+                        new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                        new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
+                        4.5, // Max module speed, in m/s
+                        0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+                        new ReplanningConfig() // Default path replanning config. See the API for the options here
+                ),
+                () -> {
+                    // Boolean supplier that controls when the path will be mirrored for the red
+                    // alliance
+                    // This will flip the path being followed to the red side of the field.
+                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
 
+                    var alliance = DriverStation.getAlliance();
+                    if (alliance.isPresent()) {
+                        return alliance.get() == DriverStation.Alliance.Red;
+                    }
+                    return false;
+                },
+                this // Reference to this subsystem to set requirements
+        );
     }
 
     GenericEntry gyroAngle = Shuffleboard.getTab("swerve").add("gyroAngle", 0).getEntry();
@@ -45,10 +80,15 @@ public class DriveTrain extends SubsystemBase {
     public static final double kMaxSpeed = 3.0; // 3 meters per second
 
     public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
-        var swerveModuleStates = KINEMATICS.toSwerveModuleStates(
+        driveRobotRelative(
                 fieldRelative
                         ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, getGyroscopeRotation())
                         : new ChassisSpeeds(xSpeed, ySpeed, rot));
+    }
+
+
+    public void driveRobotRelative(ChassisSpeeds speed) {
+        var swerveModuleStates = KINEMATICS.toSwerveModuleStates(speed);
         SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, kMaxSpeed);
         m_frontLeft.setDesiredState(swerveModuleStates[0]);
         m_frontRight.setDesiredState(swerveModuleStates[1]);
@@ -59,10 +99,14 @@ public class DriveTrain extends SubsystemBase {
     private final Field2d m_field = new Field2d();
     private final Pose2d m_startPose = new Pose2d(0, 0, Rotation2d.fromDegrees(0));
 
+    public void resetPose(Pose2d pose) {
+        poseEstimator.resetPosition(getGyroscopeRotation(), getModulePositions(), m_startPose);
+    }
+
     public Command zeroCommand() {
         return this.runOnce(() -> {
             navx.reset();
-poseEstimator.resetPosition(getGyroscopeRotation(), getModulePositions() ,m_startPose);
+            poseEstimator.resetPosition(getGyroscopeRotation(), getModulePositions(), m_startPose);
         });
     }
 
@@ -71,20 +115,23 @@ poseEstimator.resetPosition(getGyroscopeRotation(), getModulePositions() ,m_star
             this.drive(xspeed.getAsDouble(), yspeed.getAsDouble(), rot.getAsDouble(), true);
         });
     }
-    
-    //public static final double DT_FL_SE_OFFSET = 155.302734375 - 90;
+    public ChassisSpeeds getSpeeds() {
+        return KINEMATICS.toChassisSpeeds(getModuleStates());
+      }
+
+    // public static final double DT_FL_SE_OFFSET = 155.302734375 - 90;
     public static final double DT_FL_SE_OFFSET = 38.84765625 - 90;
 
     // Steer CANcoder offset front right
-    //public static final double DT_FR_SE_OFFSET = 124.98046875 - 90;
+    // public static final double DT_FR_SE_OFFSET = 124.98046875 - 90;
     public static final double DT_FR_SE_OFFSET = 153.80859375 - 90;
 
     // Steer CANcoder offset back left
-    //public static final double DT_BL_SE_OFFSET = 8.96484375 - 90;
+    // public static final double DT_BL_SE_OFFSET = 8.96484375 - 90;
     public static final double DT_BL_SE_OFFSET = 346.81640625 - 90;
 
     // Steer CANcoder offset back right
-    //public static final double DT_BR_SE_OFFSET = 247.5 - 90;
+    // public static final double DT_BR_SE_OFFSET = 247.5 - 90;
     public static final double DT_BR_SE_OFFSET = 352.705078125 - 90;
 
     private final SwerveModule m_frontLeft = new SwerveModule("frontleft", 12, 11, 1, DT_FL_SE_OFFSET);
@@ -101,7 +148,7 @@ poseEstimator.resetPosition(getGyroscopeRotation(), getModulePositions() ,m_star
 
     private Pose2d getPose() {
         var pose = poseEstimator.getEstimatedPosition();
-        return new Pose2d(pose.getY(), pose.getX() *-1, pose.getRotation());
+        return new Pose2d(pose.getY(), pose.getX() * -1, pose.getRotation());
     }
 
     AHRS navx = new AHRS();
@@ -118,6 +165,14 @@ poseEstimator.resetPosition(getGyroscopeRotation(), getModulePositions() ,m_star
                 m_frontRight.getPosition(),
                 m_backLeft.getPosition(),
                 m_backRight.getPosition()
+        };
+    }
+    public SwerveModuleState[] getModuleStates() {
+        return new SwerveModuleState[] {
+                m_frontLeft.getState(),
+                m_frontRight.getState(),
+                m_backLeft.getState(),
+                m_backRight.getState()
         };
     }
 
@@ -159,10 +214,10 @@ poseEstimator.resetPosition(getGyroscopeRotation(), getModulePositions() ,m_star
 
     public Command xcommand() {
         return this.run(() -> {
-        m_backLeft.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(-135)));
-        m_frontLeft.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(-45)));
-        m_backRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(135)));
-        m_frontRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
+            m_backLeft.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(-135)));
+            m_frontLeft.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(-45)));
+            m_backRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(135)));
+            m_frontRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
         });
     }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,10 @@
 
 package frc.robot;
 
+import java.util.List;
 import frc.lib.CommandChooser;
+import frc.lib.CommandXBoxWrapper;
+import frc.lib.GetMACAddress;
 import frc.lib.PDHLogPowerFaults;
 import frc.lib.leds.LEDs;
 import frc.lib.leds.LedSegment;
@@ -13,75 +16,98 @@ import frc.robot.Constants.OperatorConstants;
 
 import com.ctre.phoenix.led.CANdle;
 import com.ctre.phoenix.led.StrobeAnimation;
-
+import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  private final DriveTrain driveTrain = new DriveTrain();
-  private final LEDs leds;
   private CANdle left = new CANdle(Constants.LEDS.LEFT);
   private CANdle right = new CANdle(Constants.LEDS.RIGHT);
-  
+  @SuppressWarnings("unused")
+  private final LEDs m_leds;
+  private final DriveTrain driveTrain = new DriveTrain();
   // The robot's subsystems and commands are defined here...
   private final PowerDistribution m_PowerDistribution = new PowerDistribution();
   @SuppressWarnings("unused")
   public static final DriverTab drivertab = new DriverTab();
-  public final CommandChooser commandChooser = new CommandChooser();
+  private final SendableChooser<Command> autoChooser;
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
-      /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    //PDHLogPowerFaults.setPdh(m_PowerDistribution);
-    // Configure the trigger bindings
-    leds = new LEDs(
-                        new LedSegment[] {new LedSegment(left), new LedSegment(right)}, 
-                        new LedSignal[] {
-                                LedSignal.isBrownedOut(),
-                                LedSignal.isDSConnected(),
-                                // LedSignal.hasTarget(),
-                                LedSignal.isEndGame(),
-                                LedSignal.hasActiveFault(),
-                                LedSignal.getLowBatteryLedSignal(),
-                                LedSignal.previouslyHadFault(),      
-                        });
-    configureBindings();
-    driveTrain.setDefaultCommand(driveTrain.driveCommand(() -> {
-      return MathUtil.applyDeadband(m_driverController.getLeftX(), 0.1);      
-    }, () -> {
-      return MathUtil.applyDeadband(m_driverController.getLeftY() * -1, 0.1);
-    }, () -> {
-      return MathUtil.applyDeadband(m_driverController.getRightX(), 0.1);
-
-    }));
-  }
   
 
+  
+
+  private final CommandXBoxWrapper m_driverController = new CommandXBoxWrapper(OperatorConstants.kDriverControllerPort);
+
+
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    PDHLogPowerFaults.setPdh(m_PowerDistribution);
+    GetMACAddress.main();
+    // Configure the trigger bindings
+    configureBindings();
+    driveTrain.setDefaultCommand(driveTrain.driveCommand(() -> {
+      return m_driverController.getLeftX(0.1);
+    }, () -> {
+      return m_driverController.getLeftY(.1) * -1;
+    }, () -> {
+      return m_driverController.getRightX(.1);
+
+    }));
+
+    m_leds = new LEDs(
+        new LedSegment[] { new LedSegment(left), new LedSegment(right) },
+        new LedSignal[] {
+            LedSignal.isBrownedOut(),
+            LedSignal.isDSConnected(),
+            // LedSignal.hasTarget(),
+            LedSignal.isEndGame(),
+            LedSignal.hasActiveFault(),
+            LedSignal.getLowBatteryLedSignal()
+        });
+        autoChooser = AutoBuilder.buildAutoChooser();
+
+        // Another option that allows you to specify the default auto by its name
+        // autoChooser = AutoBuilder.buildAutoChooser("My Default Auto");
+
+        SmartDashboard.putData("Auto Chooser", autoChooser);
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    m_driverController.a().onTrue(driveTrain.zeroCommand());  
+    m_driverController.a().onTrue(driveTrain.zeroCommand());
     m_driverController.x().whileTrue((driveTrain.xcommand()));
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
+    // pressed,
     // cancelling on release.
   }
 
@@ -92,6 +118,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return commandChooser.getSelected();
+    return autoChooser.getSelected();
   }
 }
