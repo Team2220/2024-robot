734385a5c392afd9623b236c8406641c1669c2b4
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2024-01-25 19:45:07 -0600

    cites

--- src/main/java/frc/lib/GetMACAddress.java
+++ src/main/java/frc/lib/GetMACAddress.java
@@ -2,6 +2,8 @@ package frc.lib;
 
 import java.net.*;
 
+/* got code from here: https://www.tutorialspoint.com/java-program-to-get-system-mac-address-of-windows-and-linux-machine#:~:text=Get%20NetworkInterface%20instance%20by%20using,store%20in%20a%20byte%20array. */
+
 public class GetMACAddress {
     public static void main() {
         InetAddress address;
