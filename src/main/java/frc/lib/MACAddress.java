c6462ddbf044b02404c5bc7a14dc2c8cd6245d97
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2024-01-27 12:00:21 -0600

    macaddress op

--- src/main/java/frc/lib/MACAddress.java
+++ src/main/java/frc/lib/MACAddress.java
@@ -1,11 +1,16 @@
 package frc.lib;
 /* https://stackoverflow.com/questions/604424/how-to-get-an-enum-value-from-a-string-value-in-java */
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.function.Function;
+
 public enum MACAddress {
     Robot23("00-80-2F-17-60-67"),
     Robot24("");
 
     private String address;
+    private static MACAddress current = getMacAddress();
 
     MACAddress(String text) {
         this.address = text;
@@ -22,5 +27,37 @@ public enum MACAddress {
             }
         }
         return null;
+    }
+
+    public static String getMacAddressStr() {
+        InetAddress address;
+        try {
+            address = InetAddress.getLocalHost();
+            NetworkInterface networkInterface = NetworkInterface.getByInetAddress(address);
+            byte[] mac = networkInterface.getHardwareAddress();
+            System.out.print("MAC address  : ");
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < mac.length; i++) {
+                stringBuilder.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+            }
+            return stringBuilder.toString();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return "Error";
+        }
+    }
+
+    public static MACAddress getMacAddress() {
+        var check = fromString(getMacAddressStr());
+        if (check == null) {
+            return Robot24;
+        } else {
+            return check;
+        }
+    }
+
+    public static <T> void conifg(Function<MACAddress, T> config) {
+        config.apply(current);
     }
 }
