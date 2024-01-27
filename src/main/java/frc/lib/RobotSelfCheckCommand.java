862e97180028167e50259c75dad849ac78fb7db4
ShreeVashistha <146397661+ShreeVashistha2010@users.noreply.github.com>
2024-01-27 14:46:02 -0600

    Update RobotSelfCheckCommand.java

--- src/main/java/frc/lib/RobotSelfCheckCommand.java
+++ src/main/java/frc/lib/RobotSelfCheckCommand.java
@@ -3,5 +3,8 @@ package frc.lib;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class RobotSelfCheckCommand extends SequentialCommandGroup{
-    
+    public RobotSelfCheckCommand(CheckableSubsystem[] subsystems){
+
+    }
+
 }
