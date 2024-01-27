79a8fa87c3d73318b4d6ba46158c181db8ed943d
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2024-01-27 14:03:04 -0600

    Update Shooter.java

--- src/main/java/frc/lib/Shooter.java
+++ src/main/java/frc/lib/Shooter.java
@@ -2,24 +2,27 @@ package frc.lib;
 
 import java.util.function.DoubleSupplier;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 
-public class Shooter {
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Shooter extends SubsystemBase {
     TalonFXWrapper left;
     TalonFXWrapper right;
 
     public Shooter() {
-        left = new TalonFXWrapper(0, null);
-        right = new TalonFXWrapper(0, null);
+        left = new TalonFXWrapper(Constants.Shooter.id_left, "left");
+        right = new TalonFXWrapper(Constants.Shooter.id_right, "right");
     }
 
     public Command dutyCycleCommand(DoubleSupplier leftSpeed, DoubleSupplier rightSpeed) {
-        this.run(() -> {
-            SetControlDutyCycle duty = new SetControlDutyCycle();
-            left.setControl(DutyCycle);
-            right.setControl(DutyCycle);
+        return this.run(() -> {
+            DutyCycleOut dutyLeft = new DutyCycleOut(leftSpeed.getAsDouble());
+            DutyCycleOut dutyRight = new DutyCycleOut(rightSpeed.getAsDouble());
+            left.setControl(dutyLeft);
+            right.setControl(dutyRight);
         });
-        return null;
-
     }
 }
