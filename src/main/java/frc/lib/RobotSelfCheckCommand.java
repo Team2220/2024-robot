c91d01ccbd62b58ccf09cdc75f1aa4ea1332ff4f
alphasun532 <124937787+alphasun532@users.noreply.github.com>
2024-02-01 19:27:34 -0600

    add getTimeoutSeconds

--- src/main/java/frc/lib/RobotSelfCheckCommand.java
+++ src/main/java/frc/lib/RobotSelfCheckCommand.java
@@ -4,18 +4,19 @@ import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class RobotSelfCheckCommand extends SequentialCommandGroup {
-    public RobotSelfCheckCommand(CheckableSubsystem[] subsystems){
-    for (CheckableSubsystem subsystem : subsystems) {
-        var commands = subsystem.getCheckCommands();
-        for (CheckCommand command : commands){
-            addCommands(command.finallyDo((interrupted)->{
-                if (interrupted) 
-                    anyFailed = true;
-                 
+    public RobotSelfCheckCommand(CheckableSubsystem[] subsystems) {
+        for (CheckableSubsystem subsystem : subsystems) {
+            var commands = subsystem.getCheckCommands();
+            for (CheckCommand command : commands) {
+                addCommands(command.finallyDo((interrupted) -> {
+                    if (interrupted)
+                        anyFailed = true;
+
                     DataLogManager.log(command.getName() + "  " + subsystem.getName());
-            }));
+                }).withTimeout(command.getTimeoutSeconds()));
+            }
         }
     }
-    }
+
     public boolean anyFailed = false;
 }
