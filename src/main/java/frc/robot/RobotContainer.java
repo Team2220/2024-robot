efedd6201b85a2feec7aea14a92f3ca4fa77d5e3
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2024-02-02 20:31:12 -0600

    ...................

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -127,7 +127,7 @@ public class RobotContainer {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
     m_driverController.a().onTrue(driveTrain.zeroCommand());
     m_driverController.x().whileTrue((driveTrain.xcommand()));
-    m_driverController.y().onTrue(new TalonOrchestra(driveTrain));
+    // m_driverController.y().onTrue(new TalonOrchestra(driveTrain));
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is
     // pressed,
     // cancelling on release.
@@ -142,7 +142,8 @@ public class RobotContainer {
     // An example command will be run in autonomous
     return autoChooser.getSelected();
   }
-  public Command getTestCommand() {
-    return new RobotSelfCheckCommand(driveTrain);
+
+  public void getTestCommand() {
+    // return new RobotSelfCheckCommand(driveTrain);
   }
 }
