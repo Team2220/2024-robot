e2aa564ddcb5ba03961709ae03a2814162a4c68e
nerdguy22 <danielson.asher@gmail.com>
2024-02-03 15:08:44 -0600

    Update MusicToneCommand.java

--- src/main/java/frc/lib/MusicToneCommand.java
+++ src/main/java/frc/lib/MusicToneCommand.java
@@ -1,5 +1,23 @@
 package frc.lib;
 
-public class MusicToneCommand {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class MusicToneCommand extends Command{
+    public final List<TalonFX> talonFXs = new ArrayList<>();
+    public final double frequency;
+    public MusicToneCommand(double frequency, TalonFXSubsystem... subsystems) {
+        this.frequency = frequency;
+        addRequirements(subsystems);
+        for (TalonFXSubsystem subsystem : subsystems) {
+
+            for (TalonFX talon : subsystem.getTalonFXs()) {
+                talonFXs.add(talon);
+            }
+        }
+    }
 }
