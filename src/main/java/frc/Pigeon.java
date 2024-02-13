22cb68f7218a0505c1a68c4529b5276ed7ab394d
notJithin <123132870+notJithin@users.noreply.github.com>
2024-02-12 19:51:53 -0600

    Revert "stuff"
    This reverts commit 7f9d89ce3206ebab2edc1c7693c4c4472f584b09.

--- src/main/java/frc/Pigeon.java
+++
@@ -1,8 +0,0 @@
-package frc;
-
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.sensors.WPI_PigeonIMU;
-
-public class Pigeon {
-    WPI_PigeonIMU gyro = new WPI_PigeonIMU(0); // Pigeon is on CAN Bus with device ID 0
-}
