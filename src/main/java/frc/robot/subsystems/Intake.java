37c917643842e64d1206afa1c7055ba9cc0aca4c
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2024-02-14 19:52:34 -0600

    implement sparkmaxwrapper in intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -3,44 +3,29 @@ package frc.robot.subsystems;
 import java.util.function.DoubleSupplier;
 
 import com.ctre.phoenix6.controls.DutyCycleOut;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.SparkPIDController;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.SparkMaxWrapper;
 import frc.lib.TalonFXWrapper;
 import frc.lib.selfCheck.CheckCommand;
 import frc.lib.selfCheck.CheckableSubsystem;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase implements CheckableSubsystem {
-    private CANSparkMax intakeRev;
-    private SparkPIDController intakePidController;
-    public double kP_intake, kI_intake, kD_intake;
-    TalonFXWrapper conveyor;
+    private SparkMaxWrapper intake;
+    private TalonFXWrapper conveyor;
 
     public Intake() {
-        intakeRev = new CANSparkMax(Constants.Intake.id_intake, MotorType.kBrushless);
-
-        intakeRev.restoreFactoryDefaults();
-
-        intakePidController = intakeRev.getPIDController();
-
-        intakePidController.setP(kP_intake);
-        intakePidController.setI(kI_intake);
-        intakePidController.setD(kD_intake);
-
-        // Shuffleboard.getTab("Intake").add("Intake PID Controller", intakePidController);
-        // conveyor = new TalonFXWrapper(Constants.Intake.id_conv, "conveyor");
+        intake = new SparkMaxWrapper(Constants.Intake.id_intake, "intake");
+        conveyor = new TalonFXWrapper(Constants.Intake.id_conv, "conveyor");
     }
 
     public Command dutyCycleCommand(DoubleSupplier speed) {
+
         return this.run(() -> {
             DutyCycleOut duty = new DutyCycleOut(speed.getAsDouble());
-             intakeRev.set(speed.getAsDouble());
-            //  conveyor.setControl(duty);
+            intake.set(speed.getAsDouble());
+            conveyor.setControl(duty);
         });
     }
 
