99724c886d0847db045362a29457ba67dfbb3c0f
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2024-02-14 20:24:36 -0600

    arm gear ratios

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -40,10 +40,7 @@ public final class Constants {
 
   public static class Arm {
     public static final int ARM_TALON = 20;
-    // public static double ARM_GEAR_RATIO =
-    // (5.0/1.0)*(4.0/1.0)*(3.0/1.0)*(18.0/58.0);
-
-    public static final double ARM_GEAR_RATIO = (5.0 / 1.0) * (5.0 / 1.0) * (5.0 / 1.0);
+    public static final double ARM_GEAR_RATIO = (5.0 / 1.0) * (4.0 / 1.0) * (3.0 / 1.0) * (18.0 / 58.0);
   }
 
   public static final class LimelightConfig {
