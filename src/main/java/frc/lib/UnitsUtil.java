ffbc128d9d231241b03d66f144d3e5748bc9d238
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2024-02-27 21:05:41 -0600

    UnitsUtil

---
+++ src/main/java/frc/lib/UnitsUtil.java
@@ -0,0 +1,30 @@
+package frc.lib;
+
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.Distance;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.Unit;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.Velocity;
+
+public class UnitsUtil<U extends Unit<U>> {
+    private UnitsUtil() {
+        // hi - dont make instances
+    }
+
+    public Measure<U> abs(Measure<U> measure) {
+        double dMeasure = measure.in(measure.unit());
+        return measure.unit().of(Math.abs(dMeasure));
+    }
+
+    public Measure<Distance> distanceForWheel(Measure<Distance> wheelDiameter, Measure<Angle> rotations) {
+        var distance = Math.PI * wheelDiameter.in(Units.Meters) * rotations.in(Units.Rotations);
+        return Units.Meters.of(distance);
+    }
+
+    public Measure<Velocity<Distance>> velocityForWheel(Measure<Velocity<Distance>> velocity) {
+        double vel = velocity.in(Units.MetersPerSecond);
+        return Units.MetersPerSecond.of(vel);
+
+    }
+}
