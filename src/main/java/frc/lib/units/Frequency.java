3dfe51b707e25995aca9665dd1dfc26d3ace2876
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2024-03-02 14:29:05 -0600

    Update Frequency.java

--- src/main/java/frc/lib/units/Frequency.java
+++ src/main/java/frc/lib/units/Frequency.java
@@ -1,8 +1,22 @@
+// Copyright (c) Ashwin Kalyan and Team 2220 Blue Twilight.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 package frc.lib.units;
 
 import edu.wpi.first.units.UnaryFunction;
 import edu.wpi.first.units.Unit;
 
+/**
+ * Unit of the number of waves that pass a fixed point in unit time.
+ *
+ * <p>
+ * This is the base type for units of frequency.
+ *
+ * <p>
+ * Actual units (such as {@link Units#hertz} and {@link Units#megaHertz}) can be
+ * found in the
+ * {@link UnitsUtil} class.
+ */
 public class Frequency extends Unit<Frequency> {
     /** Creates a new unit with the given name and multiplier to the base unit. */
     Frequency(double baseUnitEquivalent, String name, String symbol) {
