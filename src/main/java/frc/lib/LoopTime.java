cc23c1a3050596c673d80747d2a03d181bea3a04
Anvith <819178@apps.district196.org>
2024-03-07 20:06:41 -0600

    ashwin's comment

--- src/main/java/frc/lib/LoopTime.java
+++ src/main/java/frc/lib/LoopTime.java
@@ -5,11 +5,14 @@ import edu.wpi.first.units.Time;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.Timer;
 
-public class LoopTime {
-    private String name;
 
+/*
+ * This class works the same way as WPILib's Tracer class:
+ * https://github.com/wpilibsuite/allwpilib/blob/e64c20346dfe3252098f0efe51a93bb766881b82/wpilibj/src/main/java/edu/wpi/first/wpilibj/Tracer.java
+ */
+
+public class LoopTime {
     public LoopTime(String name) {
-        this.name = name;
     }
 
     public Measure<Time> measure(Runnable runable) {
