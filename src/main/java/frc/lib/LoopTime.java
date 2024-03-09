e59cc9eaf8eab2bdddb335a300b3d9fc893a00d1
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2024-03-08 19:46:56 -0600

    Update LoopTime.java
    Co-Authored-By: notJithin <123132870+notJithin@users.noreply.github.com>

--- src/main/java/frc/lib/LoopTime.java
+++ src/main/java/frc/lib/LoopTime.java
@@ -5,7 +5,6 @@ import edu.wpi.first.units.Time;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.Timer;
 
-
 /*
  * This class works the same way as WPILib's Tracer class:
  * https://github.com/wpilibsuite/allwpilib/blob/e64c20346dfe3252098f0efe51a93bb766881b82/wpilibj/src/main/java/edu/wpi/first/wpilibj/Tracer.java
