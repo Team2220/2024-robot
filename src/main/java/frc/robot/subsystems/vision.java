4ac39960c90931fb3940ccce3df1ec4af4d86020
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2024-03-08 19:47:36 -0600

    Update vision.java
    Co-Authored-By: notJithin <123132870+notJithin@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/vision.java
+++ src/main/java/frc/robot/subsystems/vision.java
@@ -3,17 +3,18 @@
 
 // Camera camera = CameraServer.getInstance().getVideo();
 // VM videoMode = camera.getVideoMode();
-// Photo  photo = new Image(videoMode.pixelFormat, videoMode.width, videoMode.height);
+// Photo photo = new Image(videoMode.pixelFormat, videoMode.width,
+// videoMode.height);
 
 // public class Robot {
 
-//     public void robotInit() {
-//         try {
-//             CameraServer.getInstance().startAutomaticCapture();
-//         } catch (Exception e) {
-//             System.err.println("We have a bit of a problem dog: " + e.getMessage());
-//         }
-//     }
+// public void robotInit() {
+// try {
+// CameraServer.getInstance().startAutomaticCapture();
+// } catch (Exception e) {
+// System.err.println("We have a bit of a problem dog: " + e.getMessage());
+// }
+// }
 // }
 
 // AprilTagDetector detector = new AprilTagDetector();
