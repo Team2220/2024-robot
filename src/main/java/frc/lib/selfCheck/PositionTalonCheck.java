c74c05b817c38215fd763a90d64551d7e85c09b7
alphasun532 <124937787+alphasun532@users.noreply.github.com>
2024-03-11 20:52:56 -0500

    Stuff Ashwin

---
+++ src/main/java/frc/lib/selfCheck/PositionTalonCheck.java
@@ -0,0 +1,38 @@
+package frc.lib.selfCheck;
+
+import java.util.function.DoubleConsumer;
+
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.Measure;
+import frc.lib.TalonFXWrapper;
+
+public class PositionTalonCheck extends CheckCommand {
+    TalonFXWrapper talon;
+    Measure<Angle> position;
+    Measure<Angle> tolerance;
+
+    public PositionTalonCheck(TalonFXWrapper talon, Measure<Angle> position, Measure<Angle> tolerance) {
+        this.talon = talon;
+        this.position = position;
+        this.tolerance = tolerance;
+    }
+
+    @Override
+    public void execute() {
+        talon.setMotionMagicVoltage(position);
+    }
+
+    @Override
+    public double getTimeoutSeconds() {
+        return 5;
+    }
+
+    @Override
+    public String getDescription() {
+       return "set Position Of " + talon.getName() + " to " + position.toLongString();
+    }
+    @Override
+    public boolean isFinished() {
+        return talon.isAtPositionReference(position, tolerance);
+    }
+}
