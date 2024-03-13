32fab5de244b1d517d716d62a03df61c059937d2
Anvith <819178@apps.district196.org>
2024-03-12 19:11:18 -0500

    max time fault ashween

--- src/main/java/frc/lib/LoopTimer.java
+++ src/main/java/frc/lib/LoopTimer.java
@@ -4,19 +4,24 @@ import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import frc.lib.faults.Fault;
 
 /*
  * This class works the same way as WPILib's Tracer class:
  * https://github.com/wpilibsuite/allwpilib/blob/e64c20346dfe3252098f0efe51a93bb766881b82/wpilibj/src/main/java/edu/wpi/first/wpilibj/Tracer.java
  */
 
-public class LoopTime {
+public class LoopTimer {
     private String name;
     private GenericEntry graph;
+    private Fault fault;
+    private double maxTimeMilliseconds;
 
-    public LoopTime(String name) {
+    public LoopTimer(String name, double maxTimeMilliseconds) {
         this.name = name;
         this.graph = Shuffleboard.getTab("LoopTime").add(name, 0).withWidget(BuiltInWidgets.kGraph).getEntry();
+        this.maxTimeMilliseconds = maxTimeMilliseconds;
+        this.fault = new Fault(name + " exceeded " + maxTimeMilliseconds);
     }
 
     public void measure(Runnable runable) {
@@ -24,5 +29,7 @@ public class LoopTime {
         runable.run();
         double end = Timer.getFPGATimestamp();
         graph.setDouble((end - start) * 1000);
+        boolean isMaxTimeExceeded = (end - start) * 1000 > maxTimeMilliseconds;
+        fault.setIsActive(isMaxTimeExceeded);
     }
 }
