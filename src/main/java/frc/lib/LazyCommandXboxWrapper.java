99296e61b03446f418c2c5f372bc58ab9ed21319
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2024-03-12 20:45:13 -0500

    Update LazyCommandXboxWrapper.java

--- src/main/java/frc/lib/LazyCommandXboxWrapper.java
+++ src/main/java/frc/lib/LazyCommandXboxWrapper.java
@@ -5,7 +5,6 @@ import edu.wpi.first.wpilibj.event.EventLoop;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.lib.faults.Fault;
 
 public class LazyCommandXboxWrapper {
 
