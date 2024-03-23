e2dbcd6487021d97c020366743ef91ceda91bcea
notJithin <123132870+notJithin@users.noreply.github.com>
2024-03-23 15:10:44 -0500

    ji

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -78,11 +78,11 @@ public class RobotContainer {
 
     NamedCommands.registerCommand("armSpeakerPos", m_arm.setPositionOnceCommand(55));
     NamedCommands.registerCommand("firstArmSpeakerPos", m_arm.setPositionOnceCommand(55).withTimeout(2));
-    NamedCommands.registerCommand("armRestFull", m_arm.setPositionOnceCommand(0));
+    NamedCommands.registerCommand("armRestFull", m_arm.setPositionOnceCommand(0).withTimeout(2));
     NamedCommands.registerCommand("armRest", m_arm.setPositionOnceCommand(15));
     NamedCommands.registerCommand("3.1", m_arm.setPositionOnceCommand(31));
     NamedCommands.registerCommand("3.2", m_arm.setPositionOnceCommand(33));
-    NamedCommands.registerCommand("3.3", m_arm.setPositionOnceCommand(31));
+    NamedCommands.registerCommand("3.3", m_arm.setPositionOnceCommand(31).withTimeout(2));
     NamedCommands.registerCommand("3.4", m_arm.setPositionOnceCommand(40));
     NamedCommands.registerCommand("saboStart", m_arm.setPositionOnceCommand(46));
     NamedCommands.registerCommand("armIntake", m_arm.setPositionOnceCommand(20).andThen(intake.setIntakeUntilQueued()));
