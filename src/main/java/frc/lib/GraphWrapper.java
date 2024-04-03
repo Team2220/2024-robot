f7a12dc965c120f11710328b6a0ae8eb86a036fb
Ashwin Kalyan <ashwinkalyan2@gmail.com>
2024-04-02 19:26:33 -0500

    Create GraphWrapper.java

---
+++ src/main/java/frc/lib/GraphWrapper.java
@@ -0,0 +1,25 @@
+package frc.lib;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import frc.robot.Constants;
+
+public class GraphWrapper {
+    private String name;
+    private GenericEntry graph;
+    private String tab;
+
+    public GraphWrapper(String name, String tab) {
+        this.name = name;
+        if (Constants.isGraphsEnabled) {
+            this.graph = Shuffleboard.getTab(tab).add(name,
+                    0).withWidget(BuiltInWidgets.kGraph).getEntry();
+        }
+    }
+
+    public void setDouble(double value) {
+        if (graph != null)
+            graph.setDouble(value);
+    }
+}
