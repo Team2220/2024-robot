824ce486e853ad3d1304f5e6fd4f3c3328db21ce
notGriffin <krima724@gmail.com>
2024-04-06 18:46:11 -0700

    code

--- src/main/java/frc/lib/leds/LedAnimation.java
+++ src/main/java/frc/lib/leds/LedAnimation.java
@@ -1,22 +1,22 @@
 package frc.lib.leds;
 
-class LedAnimation{
+import java.util.function.Consumer;
 
+import com.ctre.phoenix.led.Animation;
+import com.ctre.phoenix.led.CANdle;
 
+class LedAnimation {
+    private Consumer<CANdle> run;
 
+    public LedAnimation(Consumer<CANdle> run) {
+        this.run = run;
+    }
 
+    public LedAnimation(Animation animation) {
+        this((candle) -> { 
+            candle.animate(animation);
 
-
-
-
-
-
-
-
-
-
-
-
-
-    
+        });
+    }
+    // this is the end of the class
 }
