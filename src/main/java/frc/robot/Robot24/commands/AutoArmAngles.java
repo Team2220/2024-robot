ab43b802d72f707c7b191fd0ed474a6ee2e4f908
Addie <99558988+addielyon@users.noreply.github.com>
2024-11-02 14:24:40 -0500

    AutoArmAngle working

--- src/main/java/frc/robot/Robot24/commands/AutoArmAngles.java
+++ src/main/java/frc/robot/Robot24/commands/AutoArmAngles.java
@@ -1,5 +1,7 @@
 package frc.robot.Robot24.commands;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.controller.PIDController;
@@ -16,6 +18,9 @@ import frc.robot.Robot24.subsystems.Arm;
 public class Angles extends Command {
     Arm arm;
     DriveTrain driveTrain;
+    DoubleSupplier xSupplier;
+    DoubleSupplier ySupplier;
+    DoubleSupplier rotSupplier;
     private AprilTagFieldLayout layout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
     Pose3d RED = layout.getTagPose(4).get();
     Pose3d BLUE = layout.getTagPose(7).get();
@@ -25,15 +30,18 @@ public class Angles extends Command {
     private static TunableDouble d = new TunableDouble("D", 0.00001, true, "limelight");
     InterpolatingDoubleTreeMap map = new InterpolatingDoubleTreeMap();
 
-    public Angles(Arm arm, DriveTrain driveTrain) {
+    public Angles(Arm arm, DriveTrain driveTrain, DoubleSupplier xSupplier, DoubleSupplier ySupplier, DoubleSupplier rotSupplier) {
         addRequirements(driveTrain, arm);
         this.arm = arm;
         this.driveTrain = driveTrain;
+        this.xSupplier = xSupplier;
+        this.ySupplier = ySupplier;
+        this.rotSupplier = rotSupplier;
         map.put(0.0, 63.0);
-        map.put(1.0, 60.0);
-        map.put(3.0, 50.0);
-        map.put(5.0, 40.0);
-        map.put(7.0, 30.0);
+        map.put(1.0, 40.0);
+        map.put(3.0, 20.0);
+        map.put(5.0, 10.0);
+        map.put(7.0, 0.0);
         turningPid.setTolerance(5);
         // turningPid.setSetpoint(0);
     }
@@ -42,7 +50,6 @@ public class Angles extends Command {
     public void execute() {
         turningPid.setPID(p.getValue(), i.getValue(), d.getValue());
         double out = 0;
-
 
         var goal = DriverStation.getAlliance().get() == DriverStation.Alliance.Red ? RED : BLUE;
         driveTrain.getPose();
@@ -56,13 +63,13 @@ public class Angles extends Command {
 
                 )));
 
-        driveTrain.drive(0, 0, -out, false);
+        driveTrain.drive(xSupplier.getAsDouble(), ySupplier.getAsDouble(), rotSupplier.getAsDouble(), true);
         double armAngle = map.get(DistanceToSpeaker);
         // double speakerhight = 6.6;
         // double armAngle = Math.toDegrees(Math.atan2(speakerhight, DistanceToSpeaker))
         // * .83;
-        // System.out.println("Arm angle: " + armAngle);
-        // System.out.println("Distance to speaker: " + DistanceToSpeaker);
+        System.out.println("Arm angle: " + armAngle);
+        System.out.println("Distance to speaker: " + DistanceToSpeaker);
         arm.setPosition(armAngle);
 
     }
