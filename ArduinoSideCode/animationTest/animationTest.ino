537afde6f3ac86c12933e73e9970f7831201fdd1
scuyul <krima724@gmail.com>
2024-11-06 19:39:26 -0600

    i optimized da code
    i optimized da code

--- ArduinoSideCode/animationTest/animationTest.ino
+++ ArduinoSideCode/animationTest/animationTest.ino
@@ -9,13 +9,26 @@ const unsigned long interval = 150;  // 1 second interval in milliseconds
 unsigned long previousMillis = 0;
 bool toggleState = false;  // Track which command is active
 
+
+
 void setup() {
   Serial.begin(9600);
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
 }
 
-void loop() {
+void command1() {
+  fill_solid(leds, NUM_LEDS, CRGB(255, 0, 0));
+  FastLED.show();
+}
 
+void command2() {
+  fill_solid(leds, NUM_LEDS, CRGB(0, 0, 255));
+  FastLED.show();
+}
+
+
+void loop() {
+ //If the byte is 0x12 (i.e. 18 in decimal)
   unsigned long currentMillis = millis();
 
   if (Serial.available()) {
@@ -23,49 +36,45 @@ void loop() {
     value = Serial.read();
   }
 
-  //If the byte is 0x12 (i.e. 18 in decimal)
-  if (value == 0x12) {
-    fill_solid(leds, NUM_LEDS, CRGB(0, 0, 0));
-  }
-
-  if (value == 0x13) {
-
-    fill_solid(leds, NUM_LEDS, CRGB(255, 0, 0));
-  }
-  if (value == 0x14) {
-
-    fill_solid(leds, NUM_LEDS, CRGB(255, 140, 0));
-  }
-  if (value == 0x15) {
-
-    fill_solid(leds, NUM_LEDS, CRGB(255, 237, 0));
-  }
-
-  if (value == 0x16) {
-
-    fill_solid(leds, NUM_LEDS, CRGB(0, 128, 38));
-  }
-
-  if (value == 0x17) {
-
-    fill_solid(leds, NUM_LEDS, CRGB(0, 0, 255));
-  }
-
-  if (value == 0x18) {
-
-    fill_solid(leds, NUM_LEDS, CRGB(255, 198, 200));
-  }
-
-  if (value == 0x19) {
-
-    fill_solid(leds, NUM_LEDS, CRGB(128, 0, 128));
-  }
-
-  if (value == 0x20) {
 
 
-
-    if (currentMillis - previousMillis >= interval) {
+  switch (value)
+  {
+    case 0x12:
+    {
+      fill_solid(leds, NUM_LEDS, CRGB(255, 0, 0));
+    }break;
+    case 0x13:
+    {
+      fill_solid(leds, NUM_LEDS, CRGB(255, 0, 0));
+    }break;
+    case 0x14:
+    { 
+      fill_solid(leds, NUM_LEDS, CRGB(255, 0, 0));
+    }break;
+    case 0x15:
+    {
+      fill_solid(leds, NUM_LEDS, CRGB(0, 0, 0));
+    }break;
+    case 0x16:
+    {
+      fill_solid(leds, NUM_LEDS, CRGB(0, 0, 0));
+    }break;
+    case 0x17:
+    {
+      fill_solid(leds, NUM_LEDS, CRGB(0, 0, 0));
+    }break;
+    case 0x18:
+    {
+      fill_solid(leds, NUM_LEDS, CRGB(0, 0, 0));
+    }break;
+    case 0x19:
+    {
+      fill_solid(leds, NUM_LEDS, CRGB(0, 0, 0));
+    }break;
+    case 0x20:
+    {
+          if (currentMillis - previousMillis >= interval) {
       previousMillis = currentMillis;
       toggleState = !toggleState;
 
@@ -78,16 +87,4 @@ void loop() {
   }
   FastLED.show();
 }
-
-
-
-
-void command1() {
-  fill_solid(leds, NUM_LEDS, CRGB(255, 0, 0));
-  FastLED.show();
-}
-
-void command2() {
-  fill_solid(leds, NUM_LEDS, CRGB(0, 0, 255));
-  FastLED.show();
 }
